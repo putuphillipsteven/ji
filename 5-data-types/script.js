bf249c1510f9ca2436976bfb946a7d32993facd6
putuphillipsteven <putu.phillip@gmail.com>
2025-08-19 21:51:02 +0700

    simple learning

--- 5-data-types/script.js
+++ 5-data-types/script.js
@@ -8,10 +8,37 @@ let name = "John";
 
 let greetingMessage = "Hello " + name
 
-alert(greetingMessage)
+// alert(greetingMessage)
 
 let nameSecond = "Doe"
 
 let greetingMessageSecond = `Hello ${nameSecond} - [greetingMessageSecond]`
 
-alert(greetingMessageSecond)
+// alert(greetingMessageSecond)
+
+// Boolean
+
+let nameFieldChecked = true;
+let ageFieldChecked = true;
+
+console.log(typeof nameFieldChecked);
+
+let isGreater = 4 > 1;
+
+console.log(typeof isGreater);
+
+let age = null;
+
+console.log(typeof age);
+
+console.log(age)
+
+let ageSecond;
+
+console.log(ageSecond);
+
+let ageThird = 100;
+
+ageThird = undefined;
+
+console.log("ageThird - ", ageThird)
