d0af74ebac88586838db34444b74fb2a2e648b82
putuphillipsteven <putu.phillip@gmail.com>
2025-08-22 22:46:33 +0700

    my fault not start early

---
+++ 8-basic-operators-maths/script.js
@@ -0,0 +1,40 @@
+// Unary, binary, and Operand
+
+// Operand is what operators are applied to
+
+let xOne = 5 * 2;
+
+// It have two operands, the left operand is 5, and the right
+// operand is 2. Some people call this an arguments.
+
+// An operator is unary if it has a single operand.
+
+let xTwo = 1;
+
+xTwo = -xTwo;
+
+console.log(xTwo);
+
+// The unary negation [-] reverses the sign of a number
+
+// An operator is binary if it has two operands
+
+let xThree = 1;
+
+let yOne = 3;
+
+console.log(xThree - yOne);
+
+console.log(8 % 3);
+
+console.log(2 ** 5);
+
+console.log("John" + " Doe");
+
+console.log("2" + 1);
+
+console.log("2" + 3 * 10);
+
+console.log("2" + 3 + 10);
+
+console.log(3 * 10 + "2");
